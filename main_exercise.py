83971504251d7737992d36e20fd41fc224c0317a
Anastasiia Tsapiv <nasta.tsv@gmail.com>
2025-03-12 22:05:26 +0200

    Add new exercises 2

--- main_exercise.py
+++ main_exercise.py
@@ -1,65 +1,46 @@
 import unittest
 
-
-def check_monotonic_array(array):
-    is_increase = True
-    is_decrease = True
-    uncorrect_in = []
-    uncorrect_de = []
-    n = len(array)
-    for i in range(0, n - 1):
+def get_increase_intervals(array):
+    i = 0
+    result = []
+    for j in range(1, len(array)):
+        if array[j - 1] <= array[j]:
+            if j == len(array) - 1:
+                result.append([i, j])
         
-        if array[i] > array[i + 1]:
-            
-            is_increase = False
-            uncorrect_in.append(i + 1)
-            
-    
-        elif array[i] < array[i + 1]:
-            
-            is_decrease = False
-            uncorrect_de.append(i + 1)
-            
-            
-          
-    return is_increase or is_decrease, uncorrect_in, uncorrect_de
+        else:
+            if i != j - 1:
+                result.append([i, j - 1])
+        
+            i = j
     
 
+    return result
 
-# fail1 = check_monotonic_array([1, 5, 48, 30, 32, 90, 56, 100])
-# print(fail1)
-
-# fail2 = check_monotonic_array([67, 54, 48, 89, 78, 15, 14, 100])
-# print(fail2)
-
-# fail3 = check_monotonic_array([90, 101, 126, 126, 203])
-# print(fail3)
-
-#Розділена функція
-# def fail_increase(array):
-#     n = len(array)
-#     uncorrect = []
-#     for i in range(0, n - 1):
-#         if array[i] >= array[i + 1]:
-#             array[i], array[i + 1] = array[i + 1], array[i]
-
-#             uncorrect.append(i + 1)
-
-#     return uncorrect
-
-
-# def fail_decrease(array):
-#     n = len(array)
-#     uncorrect = []
-#     for i in range(0, n - 1):
-#         if array[i] <= array[i + 1]:
-#             array[i], array[i + 1] = array[i + 1], array[i]
-
-#             uncorrect.append(i + 1)
+def get_decrease_intervals(array):
+    i = 0
+    result = []
+    for j in range(1, len(array)):
+        if array[j - 1] >= array[j]:
+            if j == len(array) - 1:
+                result.append([i, j])
         
-#     return uncorrect
+        else:
+            if i != j - 1:
+                result.append([i, j - 1])
+        
+            i = j
+    
+
+    return result
+array =  [1, 2, 3, 4, 3, 2, 5]
+
+inc_interval = get_increase_intervals(array)
+dec_interval = get_decrease_intervals(array)
+
+    
 
 
-
-
+print("Increasing interval: ", inc_interval )
+print("Decreasing interval: ", dec_interval)
 
--- main_test.py
+++ main_test.py
@@ -10,7 +10,7 @@ class TestArrayFuction(unittest.TestCase):
         self.assertTrue(check_monotonic_array([14, 4, 3, 2, 1]))
 
     def test_not_monoton(self):
-        self.assertTrue(check_monotonic_array([1, 5, 48, 30, 32, 90, 56, 100]), (False, [3, 6], [1, 2, 4, 5, 7]))
+        self.assertTrue(check_monotonic_array([1, 5, 48, 30, 57, 90, 56, 100]), (False, [3, 6], [1, 2, 4, 5, 7]))
     
     def test_not_monoton1(self):
         self.assertTrue(check_monotonic_array([67, 54, 48, 89, 78, 15, 14, 100]), (False, [1, 2, 4, 5, 6], [3, 7]))
