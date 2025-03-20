2abe381828bf10bc746e89d16284d9980e1557c6
Anastasiia Tsapiv <nasta.tsv@gmail.com>
2025-03-21 00:05:11 +0200

    Add folders, venv

---
+++ test/test.py
@@ -0,0 +1,22 @@
+import unittest
+from main import is_tree_balanced, BinaryTree
+
+class TestBinaryTree(unittest.TestCase):
+    def test_tree_balanced(self):
+        root = BinaryTree(3)
+        root.left = BinaryTree(9)
+        root.right = BinaryTree(20)
+        root.left.left = BinaryTree(8)
+        self.assertTrue(is_tree_balanced(root))
+
+    def test_tree_balanced222(self):
+        root = BinaryTree(24)
+        root.left = BinaryTree(18)
+        root.right = BinaryTree(70)
+        root.left.left = BinaryTree(7)
+        root.left.right = BinaryTree(50)
+        root.right.left = BinaryTree(64)
+        self.assertTrue(is_tree_balanced(root))
+
+    if __name__ == '__main__':
+        unittest.main()
