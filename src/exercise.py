3588cfcc2b91613079189738850f4b0eb9b0b445
Anastasiia Tsapiv <nasta.tsv@gmail.com>
2025-04-10 22:14:22 +0300

    add exercise(coordinates)

--- src/exercise.py
+++ src/exercise.py
@@ -6,22 +6,27 @@ def bfs(n, start_x, start_y, final_x, final_y):
     visited = set()
     row = [2, 2, -2, -2, 1, 1, -1, -1]
     col = [-1, 1, 1, -1, 2, -2, 2, -2]
-    level = 0
-    queue = deque([(start_x, start_y, level)])
+    queue = deque()
+    queue.append((start_x, start_y, 0, [(start_x, start_y)]))
+
     while queue:
-        current_x, current_y, level = queue.popleft()
-        if [current_x, current_y] == [final_x, final_y]:
+        current_x, current_y, level, path = queue.popleft()
+        if (current_x, current_y) == (final_x, final_y):
+            print("Шлях коня:", path)
             return level
+        if (current_x, current_y) in visited:
+            continue
+
+        visited.add((current_x, current_y))
+
         for k in range(n):
             new_x = current_x + row[k]
             new_y = current_y + col[k]
-            if 0 <= new_x < n and 0 <= new_y < n and (new_x, new_y) not in visited:
-                visited.add((new_x, new_y))
-                queue.append((new_x, new_y, level + 1))
+            if 0 <= new_x < n and 0 <= new_y < n:
+                new_path = path + [(new_x, new_y)]
+                queue.append((new_x, new_y, level + 1, new_path))
 
     return -1
-
-
 # print(bfs(8, 7, 0, 0, 7))
 file_path = "input.txt"
 if os.stat(file_path).st_size == 0:
